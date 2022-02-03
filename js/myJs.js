f17c97b60600647f39b10524de00d70537678e74
Duynnnn <98943236+Duynnnn@users.noreply.github.com>
2022-02-03 17:22:44 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -11,7 +11,7 @@ const textConfig = {
   text10: "Ỏooo em biết mà hí hí",
   text11:
     "Chúc người yêu của em Valentine vui vẻ nha. Yêu anh nhất ♥",
-  text12: "Ấn vào đây đi (Pass: yeuVTKH)",
+  text12: "Ấn vào đây đi (Pass: số ngày yêu nhau)",
 };
 
 $(document).ready(function () {
